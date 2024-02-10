9fa3a7bf590fd4208753c556402cfa4e9d816e7a
Brian Team <brian.team.jr@gmail.com>
2024-02-10 19:55:42 +0000

    Support eth. to DMX rev. 2.0

--- wled00/network.cpp
+++ wled00/network.cpp
@@ -123,7 +123,18 @@ const ethernet_settings ethernetBoards[] = {
     18,                   // eth_mdio,
     ETH_PHY_LAN8720,      // eth_type,
     ETH_CLOCK_GPIO17_OUT  // eth_clk_mode
-  }
+  },
+
+  // ESP ETHERNET TO DMX REV 2.0 - @SECOND-STRING
+  {
+      1,                  // eth_address,
+      5,                  // eth_power,
+      23,                 // eth_mdc,
+      18,                 // eth_mdio,
+      ETH_PHY_LAN8720,    // eth_type,
+      ETH_CLOCK_GPIO0_IN  // eth_clk_mode
+  },
+
 };
 #endif
 
